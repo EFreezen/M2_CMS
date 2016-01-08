c74c4b6d3132a61333aac8c2349892ffe005c416
EFreezen <Eduard.Friesen9292@gmx.de>
2016-01-08 21:01:22 +0100

    Update

--- alt/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-ActiveRecord\Config::initialize(function($cfg)
-{
-     $cfg->set_connections(array('development' =>'mysql://root@localhost/user_login'));
- });
--- inc/test.php
+++ inc/test.php
@@ -5,5 +5,3 @@
  * Date: 08.01.2016
  * Time: 20:09
  */
-
-//testing_commit
